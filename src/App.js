bd72d0c66cfcd7f541962ac0146aba6546715cc4
elinordin <elin.evelina@live.se>
2021-04-12 16:12:08 +0200

    Add startCountDown function and pass props to change minutes and seconds to form

--- src/App.js
+++ src/App.js
@@ -3,13 +3,46 @@ import './App.css';
 import Form from './components/form.jsx'
 
 function App() {
-  const [timer, serTimer] = useState({minutes: 0, seconds: 10})
+  const [timer, setTimer] = useState({ minutes: '0', seconds: '10' })
+  const [isTimerRunning, setIsTimerRunning] = useState(false)
+  const [timeLeft, setTimeLeft] = ({ minutes: '', seconds: '' })
+
+  const changeMinutes = (e) => { setTimer({ ...timer, minutes: e.target.value }) }
+  const changeSeconds = (e) => { setTimer({ ...timer, seconds: e.target.value }) }
+
+  const startCountDown = () => {
+    setIsTimerRunning(true)
+    let countDownDate = new Date()
+    countDownDate.setMinutes(countDownDate.getMinutes() + parseInt(timer.minutes))
+    countDownDate.setSeconds(countDownDate.getSeconds() + parseInt(timer.seconds))
+
+    const timerInterval = setInterval(() => {
+      let now = new Date()
+
+      let diff = countDownDate - now;
+      let minutesLeft = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+      let secondsLeft = Math.floor((diff % (1000 * 60)) / 1000);
+      setTimeLeft({ minutes: minutesLeft, seconds: secondsLeft })
+
+      if (diff < 0) {
+        clearInterval(timerInterval);
+        setIsTimerRunning(false)
+        console.log('done')
+      }
+
+    }, 1000);
+  }
+
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Pomodoro timer</h1>
-        <Form timer={timer}/>
+        {isTimerRunning ?
+          <Form timer={timer} changeMinutes={changeMinutes} changeSeconds={changeSeconds} startCountDown={startCountDown} />
+          :
+          <Form timer={timer} changeMinutes={changeMinutes} changeSeconds={changeSeconds} startCountDown={startCountDown} />
+        }
       </header>
     </div>
   );
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -6,15 +6,15 @@ function Form(props) {
           <div className='input-row'>
             <div className='label-input-wrapper'>
               <label htmlFor='minutes'>Minutes</label>
-              <input type='number' name='minutes' defaultValue={props.timer.minutes}/> 
+              <input type='number' name='minutes' min='0' defaultValue={props.timer.minutes} onChange={(e) => props.changeMinutes(e)}/> 
             </div>
             <p className='colon'>:</p>
             <div className='label-input-wrapper'>
               <label htmlFor='seconds'>Seconds</label>
-              <input type='number' name='seconds' defaultValue={props.timer.seconds}/>
+              <input type='number' name='seconds' min='1' defaultValue={props.timer.seconds} onChange={(e) => props.changeSeconds(e)}/>
             </div>
           </div>
-          <button type='button' className='timer-button'>Start</button>
+          <button type='button' className='timer-button' onClick={props.startCountDown}>Start</button>
         </form>
     );
 }
