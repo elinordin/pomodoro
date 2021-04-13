a04a00643c314f3dd58291009948d5f5541fd893
elinordin <elin.evelina@live.se>
2021-04-13 09:47:15 +0200

    Play music on start

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import './App.css';
 import Countdown from './components/countdown.jsx'
 import Form from './components/form.jsx'
 
+import useSound from 'use-sound';
+import music from './assets/classical.mp3';
+
 function App() {
   const [timer, setTimer] = useState({ minutes: '0', seconds: '10' })
   const [isTimerRunning, setIsTimerRunning] = useState(false)
+  const [play, {stop}] = useSound(music);
 
   const changeMinutes = (e) => { setTimer({ ...timer, minutes: e.target.value }) }
   const changeSeconds = (e) => { setTimer({ ...timer, seconds: e.target.value }) }
 
   const startCountDown = () => {
+    play()
     setIsTimerRunning(true)
     let countDownDate = new Date()
     countDownDate.setMinutes(countDownDate.getMinutes() + parseInt(timer.minutes))
     countDownDate.setSeconds(countDownDate.getSeconds() + parseInt(timer.seconds))
+    setTimer({ ...timer , seconds: timer.seconds - 1 })
 
     const timerInterval = setInterval(() => {
       let now = new Date()
@@ -25,6 +31,7 @@ function App() {
       setTimer({ minutes: minutesLeft, seconds: secondsLeft })
 
       if (diff < 0) {
+        stop()
         clearInterval(timerInterval);
         setTimer({minutes: '0', seconds: '10'})
         setIsTimerRunning(false)
